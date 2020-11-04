3dd26fa3c0f794ec864608b8652cdece7e4e9d1f
Daniel Vieira <danvie11@gmail.com>
2020-11-04 23:21:29 +0100

    SBO-0002 fix(material): customizing material styles

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,12 +1,33 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   imports: [
-    MatButtonModule
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatListModule,
+    MatProgressSpinnerModule,
+    MatSidenavModule,
   ],
   exports: [
-    MatButtonModule
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatListModule,
+    MatProgressSpinnerModule,
+    MatSidenavModule,
   ]
 })
 export class MaterialModule {}
