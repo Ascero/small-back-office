f600f961e6588c91fb18602217ea66ff7dd774d5
Daniel Vieira <danvie11@gmail.com>
2020-11-07 16:00:37 +0100

    SBO-0004 feat(app): interceptors

---
+++ src/app/user/auth-token.interceptor.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class AuthTokenInterceptor implements HttpInterceptor {
+  constructor(
+    private authenticationService: AuthService
+  ) {
+  }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = this.authenticationService.tokenValue;
+    if (token) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token[`token`]}`,
+        },
+      });
+    }
+
+    return next.handle(request);
+  }
+}
